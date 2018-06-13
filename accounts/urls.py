89f5caf2d2cf588662c77e549a2f2e7d06439370
Baalateja Kataru <kavesbteja@gmail.com>
2018-06-13 12:02:29 +0000

    Polished the mobile menu and added a 'change password' feature

--- accounts/urls.py
+++ accounts/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     path('signup/', views.signup, name='signup'),
     path('login/', views.login, name='login'),
     path('logout/', views.logout, name='logout'),
+    path('<int:user_id>/changepassword/', views.changepassword, name='changepassword'),
     path('<int:user_id>/notifications/', views.notifications, name='notifications'),
     path('<int:user_id>/notifications/dismiss/<int:notify_id>/', views.dismiss_notification, name='dismiss_notification')
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -4,13 +4,16 @@ from django.contrib import auth
 from django.contrib.auth.decorators import login_required
 from .models import Notification, UserProfile
 from django.http import HttpResponseRedirect
+from django.contrib.auth.hashers import check_password
+
 
 def get_notify_count(user):
     usernotifications = None
     if user.is_authenticated:
         usernotifications = Notification.objects.filter(user_to_notify=user, dismissed=False).count()
-    
+
     return usernotifications
+
 
 def signup(request):
     if request.user.is_authenticated:
@@ -19,24 +22,28 @@ def signup(request):
         if request.method == 'GET':
             return render(request, 'accounts/authenticate.html')
         elif request.method == 'POST':
-            if request.POST['username'] and request.POST['password'] and request.POST['confirmpassword'] and request.POST['email']:
+            if request.POST['username'] and request.POST['password'] and request.POST['confirmpassword'] and \
+                    request.POST['email']:
                 if request.POST['password'] == request.POST['confirmpassword']:
                     username_flag = 0
                     email_flag = 0
                     try:
                         username_users = User.objects.get(username=request.POST['username'])
                         username_flag = 1
-                        return render(request, 'accounts/authenticate.html', {'signup_error': 'That username already exists'})
+                        return render(request, 'accounts/authenticate.html',
+                                      {'signup_error': 'That username already exists'})
                     except User.DoesNotExist:
                         pass
                     try:
                         email_users = User.objects.get(email=request.POST['email'])
                         email_flag = 1
-                        return render(request, 'accounts/authenticate.html', {'signup_error': 'That email already exists'})
+                        return render(request, 'accounts/authenticate.html',
+                                      {'signup_error': 'That email already exists'})
                     except User.DoesNotExist:
                         pass
-                    
-                    user = User.objects.create_user(username=request.POST['username'], password=request.POST['password'], email=request.POST['email'])
+
+                    user = User.objects.create_user(username=request.POST['username'],
+                                                    password=request.POST['password'], email=request.POST['email'])
                     user_profile = UserProfile()
                     user_profile.user = user
                     user_profile.save()
@@ -45,10 +52,13 @@ def signup(request):
                 else:
                     return render(request, 'accounts/authenticate.html', {'signup_error': 'The passwords do not match'})
             else:
-                return render(request, 'accounts/authenticate.html', {'signup_error': 'Error, some fields are not filled'})
+                return render(request, 'accounts/authenticate.html',
+                              {'signup_error': 'Error, some fields are not filled'})
+
+
 def login(request):
     if request.user.is_authenticated:
-         return redirect('index')
+        return redirect('index')
     else:
         if request.method == 'GET':
             return render(request, 'accounts/authenticate.html')
@@ -62,21 +72,58 @@ def login(request):
                     return redirect('index')
             else:
                 return render(request, 'accounts/authenticate.html', {'login_error': 'Invalid credentials'})
-    
+
+
 def logout(request):
     if request.method == 'POST':
         auth.logout(request)
         return redirect('index')
+
+
+@login_required
+def changepassword(request, user_id):
+    if request.method == 'POST':
+        if request.user.id == user_id:
+            if request.POST['oldpassword'] and request.POST['newpassword'] and request.POST['confirmnewpassword']:
+                if request.user.check_password(request.POST['oldpassword']):
+                    if request.POST['newpassword'] == request.POST['confirmnewpassword']:
+                        user = get_object_or_404(User, pk=user_id)
+                        user.set_password(request.POST['newpassword'])
+                        user.save()
+                        return render(request, 'accounts/changepassword.html',
+                                      {'change_password_success': 'Password changed successfully',
+                                       'notification_count': get_notify_count(request.user)})
+                    else:
+                        return render(request, 'accounts/changepassword.html',
+                                      {
+                                          'change_password_error': 'Error, the new password and the confirmation password do not match',
+                                          'notification_count': get_notify_count(request.user)})
+                else:
+                    return render(request, 'accounts/changepassword.html',
+                                  {'change_password_error': 'Error, the existing password entered is incorrect',
+                                   'notification_count': get_notify_count(request.user)})
+            else:
+                return render(request, 'accounts/changepassword.html',
+                              {'change_password_error': 'Error, some fields have not been filled',
+                               'notification_count': get_notify_count(request.user)})
+        else:
+            return redirect('unauthorized')
+    elif request.method == 'GET':
+        return render(request, 'accounts/changepassword.html',
+                      {'notification_count': get_notify_count(request.user)})
+
 
 @login_required()
 def notifications(request, user_id):
     if request.user.id == user_id:
         user = get_object_or_404(User, pk=user_id)
         notify_objs = Notification.objects.filter(user_to_notify=request.user, dismissed=False)
-        
-        return render(request, 'accounts/notifications.html', {'notifications': notify_objs, 'notification_count': get_notify_count(request.user)})
+
+        return render(request, 'accounts/notifications.html',
+                      {'notifications': notify_objs, 'notification_count': get_notify_count(request.user)})
     else:
         return redirect('unauthorized')
+
 
 @login_required
 def dismiss_notification(request, user_id, notify_id):
@@ -86,6 +133,7 @@ def dismiss_notification(request, user_id, notify_id):
             notification.dismissed = True
             notification.save()
             notifications = Notification.objects.filter(user_to_notify=request.user, dismissed=False)
-            return render(request, 'accounts/notifications.html', {'notifications': notifications, 'notification_count': get_notify_count(request.user)})
+            return render(request, 'accounts/notifications.html',
+                          {'notifications': notifications, 'notification_count': get_notify_count(request.user)})
         else:
             return redirect('unauthorized')
